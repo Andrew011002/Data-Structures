51e3435d068779fc09555fbafe3790d193617dd9
andmholm <andmholm@iu.edu>
2022-10-18 20:20:00 -0400

    created static array

--- Arrays/static.py
+++ Arrays/static.py
@@ -1,4 +1,3 @@
-from ctypes import pointer
 import sys
 
 class StaticArray:
@@ -6,103 +5,122 @@ class StaticArray:
     def __init__(self, size: int, dtype: type):
         self.size = size
         self.dtype = dtype
-        self.pointer = 0
         self.length = 0
-        self.values = [None] * size
+        self.pointer = 0
+        self.array = [None] * size
 
-    def __getitem__(self, index):
-        if index < -self.size or index >= self.size:
-            raise IndexError(f"index {index} out of bounds")
-        return self.values[index]
-
-    def __setitem__(self, index, item):
-        if index < -self.size or index >= self.size:
-            raise IndexError(f"Index {index} out of bounds")
-        if type(item) != self.dtype:
-            raise TypeError(f"Incompatible type for array")
-
+    def append(self, item):
+        # full array
+        if len(self) == self.size:
+            raise ValueError("Array is full")
+        # invalid dtype
+        if type(item) is not self.dtype:
+            raise TypeError("Invalid dtype")
+        # add item updating attributes
+        self.array[self.pointer] = item
+        self.pointer += 1
         self.length += 1
-        if index >= self.pointer: 
-            self.values[self.pointer] = item
-            self.pointer += 1
-        else:
-            self.values[index] = item
 
     def insert(self, index, item):
-        if index < -self.size or index >= self.size:
-            raise IndexError(f"Index {index} out of bounds")
-        if self.__len__() == self.size:
-            raise ValueError(f"Array is full")
-        if type(item) != self.dtype:
-            raise TypeError(f"Incompatible type for array")
-        
-        self.length += 1
-        if index >= self.pointer:
-            self.values[self.pointer] = item
-            self.pointer += 1
-            return None
-        for i in range(self.size):
-            if i == index:
-                value = self.values[i]    
-                self.values[i] = item
-                while value is not None:
-                    i += 1
-                    temp = self.values[i]
-                    self.values[i] = value
-                    value = temp
-                self.pointer += 1
-    
-                return None
-                        
-    def append(self, item):
-        if self.__len__() == self.size:
-            raise ValueError(f"Array is full")
-        if type(item) != self.dtype:
-            raise TypeError("Incompatible type for array")
+        # full array
+        if len(self) == self.size:
+            raise ValueError("Array is full")
+        # index out of bounds
+        if index > self.size or index < -self.size:
+            raise IndexError("Index out of bounds")
+        # invalid dtype
+        if type(item) is not self.dtype:
+            raise TypeError("Invalid dtype")
+        # index less than (converted)
+        index = index + self.size if index < 0 else index
 
+        # place at specified location or most open position
+
+        # greater than pointer
+        if index >= self.pointer:
+            self.append(item)
+            return None
+
+        # insert item
+        tmp = self.array[index]
+        self.array[index] = item
+        index += 1
+        item = tmp
+        # shift any elements right 1
+        while item is not None and index <= self.pointer:
+            tmp = self.array[index]
+            self.array[index] = item
+            item = tmp
+            index += 1
+        # update length & pointer
         self.length += 1
-        self.values[self.pointer] = item
         self.pointer += 1
 
-    def remove(self, index=None):
-        if not self.__len__():
-            raise ValueError(f"Array is empty")
-        if index is not None:
-            if index < -self.size or index >= self.size:
-                raise IndexError(f"Index {index} out of bounds")
+    def remove(self, item):
+        # empty array
+        if not len(self):
+            raise ValueError("Array is empty")
+        # wrong dtype
+        if type(item) is not self.dtype:
+            raise TypeError("Invalid dtype")
+        
+        # locate the item
+        for i in range(self.pointer):
+            if self.array[i] == item:
+                self.array[i] = None
+                index = i
+                break
+            
+        # element not located
+        if self.array[i] is not None:
+            raise ValueError("Item not in Array")
 
-        self.length -= 1
+        # shift items left most
+        for i in range(index + 1, self.pointer + 1):
+            self.array[index] = self.array[i]
+            index += 1
+
+        # update pointer and size
         self.pointer -= 1
-        if index is None:
-            item = self.values[self.pointer]
-            self.values[self.pointer] = None
-            return item
-        if index >= self.pointer:
-            item = self.values[self.pointer]
-            self.values[self.pointer] = None
-            return item
-        for i in range(self.size):
-            if i == index:
-                item = self.values[i]
-                self.values[i] = None
-                while self.values[i] is None and i < self.pointer:
-                    value = self.values[i + 1]
-                    self.values[i] = value
-                    self.values[i + 1] = None
-                    i += 1
-                return item
-    
-    def bytes(self):
-        return sys.getsizeof(self.values)
+        self.length -= 1
+        return item
+
+    def __getitem__(self, index):
+        # index out of bounds
+        if index > self.size or index < -self.size:
+            raise IndexError("Index out of bounds")
+        return self.array[index]
+
+    def __setitem__(self, index, item):
+        # index out of bounds
+        if index > self.size or index < -self.size:
+            raise IndexError("Index out of bounds")
+        # invalid dtype
+        if type(item) is not self.dtype:
+            raise TypeError("Invalid dtype")
+        # index less than (converted)
+        index = index + self.size if index < 0 else index
+
+        # add to most open position
+        if index > self.pointer:
+            self.append(item)
+        # add at desired position
+        else:
+            self.array[index] = item
+        
+        
+
 
     def __len__(self):
         return self.length
 
     def __str__(self):
-        return f"{self.values}"
+        return str(self.array)
+    
 
 if __name__ == "__main__":
-    array = StaticArray(10, float)
+    arr = StaticArray(5, int)
+    pass
 
     
 
